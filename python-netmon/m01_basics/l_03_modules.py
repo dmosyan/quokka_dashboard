ecc6c8455a6c2d8643e93c0a1ac61e7d88997213
David Mosyan <dmosyan@gmail.com>
2022-03-07 11:58:24 -0800

    create module example

---
+++ python-netmon/m01_basics/l_03_modules.py
@@ -0,0 +1,9 @@
+from tabulate import tabulate
+from util import create_devices
+
+# MAIN PROGRAM
+if __name__ == '__main__':
+
+    devices, count = create_devices(num_subnets=5, num_devices=5)
+    print("\n", tabulate(devices, headers="keys"))
+    print(f"\n{count} devices are generated")
