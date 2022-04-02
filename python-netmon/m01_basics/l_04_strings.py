4b80bdc800edb38ea1d3a1516e7cfad462df9886
David Mosyan <dmosyan@gmail.com>
2022-04-02 01:32:14 -0700

    string operations2

--- python-netmon/m01_basics/l_04_strings.py
+++ python-netmon/m01_basics/l_04_strings.py
@@ -29,3 +29,34 @@ print(device1_list)
 device1_line = [item.strip() for item in device1_str.split(",")]
 print(device1_line)
 
+#ignoring case
+print("\n\nIGNORING CASE")
+model = "CSR100V"
+if model == "csr100v":
+    print("STRING MATCH")
+else:
+    print("DIDN'T MATCH")
+
+if model.lower() == "csr100V".lower():
+    print("STRING MATCH")
+else:
+    print("DIDN'T MATCH")
+
+#finding substring
+version = "Virtual XE Software 1085 (x86 arch), Version 1.3.5, Operating System Windows Server 2019"
+expected_version = "Version 1.3.5"
+index = version.find(expected_version)
+if index >= 0:
+    print(f"found version: {expected_version} at location {index}")
+else:
+    print(f"not found {expected_version}")
+
+#separating string components
+version_info = [part.strip() for part in version.split(",")]
+for vi_parts in version_info:
+    print(vi_parts)
+
+#enumerate function will return 2 values. Index and Item from the list
+version_info2 = version.split(",")
+for part_no, vi_parts in enumerate(version_info2):
+    print(f"Index num is: {part_no} | {vi_parts.strip()}")
